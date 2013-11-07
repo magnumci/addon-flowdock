e00a47f5f96ee9b7bcdd96ed81b6fca3c9b67076
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-11-06 22:24:08 -0600

    Tweak spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "simplecov"
+
 SimpleCov.start do
   add_filter ".bundle"
   add_filter "spec"
@@ -6,11 +7,7 @@ end
 
 $:.unshift File.expand_path("../..", __FILE__)
 
-require "webmock/rspec"
-require "json"
 require "magnum/addons/flowdock"
-
-WebMock.disable_net_connect!
 
 def fixture_path(filename=nil)
   path = File.expand_path("../fixtures", __FILE__)
